87e4ad46a001e6e8a422a9097ec757330e87adb9
Marcin Kwiatkowski <marcin@Marcins-Mac.local>
2013-06-11 17:28:34 -0700

    Initial Commit

---
+++ OtherTests/OtherTests.m
@@ -0,0 +1,36 @@
+//
+//  OtherTests.m
+//  OtherTests
+//
+//  Created by Marcin Kwiatkowski on 11/06/2013.
+//  Copyright (c) 2013 Marcin Kwiatkowski. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OtherTests : XCTestCase
+
+@end
+
+@implementation OtherTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
